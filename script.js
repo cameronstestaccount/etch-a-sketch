a8994cdd90129d7c3ace6cdbe1c484c512a79d9c
Cameron Testing <camriesen2@gmail.com>
2021-11-05 01:04:28 -0400

    Implement color change on mouseover

--- script.js
+++ script.js
@@ -4,16 +4,35 @@ const boxSize = WIDTH/GRID_SIZE;
 
 const container = document.querySelector('.container')
 
-for(let i = 0; i<16; ++i){
-    let row = document.createElement('div');
-    row.classList.add('row');
+createGrid();
 
-    for(let j = 0; j<16; ++j){
-        let box = document.createElement('div');
-        box.classList.add('box');
-        box.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`)
-        row.appendChild(box);
+function createGrid(){
+
+    // create grid of divs
+    for(let i = 0; i<16; ++i){
+        let row = document.createElement('div');
+        row.classList.add('row');
+
+        for(let j = 0; j<16; ++j){
+            let box = document.createElement('div');
+            box.classList.add('box');
+            box.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`)
+            row.appendChild(box);
+        }
+
+        container.appendChild(row);
     }
 
-    container.appendChild(row);
+    // add mouseover to all of the boxes
+    let boxes = Array.from(document.getElementsByClassName('box'));
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', changeColor)
+    });
+}
+
+function changeColor(event){
+    console.log("worked");
+    this.style.backgroundColor = 'black';
+    this.removeEventListener('mouseover', changeColor);
 }
