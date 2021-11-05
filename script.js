64c9594d7a6f7ecb4dd75399045a0d9dc620432e
Cameron Testing <camriesen2@gmail.com>
2021-11-05 01:38:46 -0400

    Implement reset button

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
-const GRID_SIZE = 16;
+const DEFAULT_GRID_SIZE = 16;
 const WIDTH = 800;
-const boxSize = WIDTH/GRID_SIZE;
 
-const container = document.querySelector('.container')
+const resetBtn = document.querySelector('.reset-btn');
 
 createGrid();
 
-function createGrid(){
+resetBtn.addEventListener('click', resetGrid);
+
+function resetGrid(){
+    let newSize = prompt('Enter new gridsize (max 100):');
+
+    if(newSize){
+        if(newSize < 1 || newSize > 100){
+            alert('Value must be between 1 and 100');
+        } else {
+            let oldGrid = document.querySelector('.container');
+            console.log(oldGrid);
+            oldGrid.remove();
+            createGrid(newSize);
+        }
+    }
+
+}
+
+function createGrid(gridSize=DEFAULT_GRID_SIZE){
+    const boxSize = WIDTH/gridSize;
+
+    const container = document.createElement('div');
+    container.classList.add('container');
 
     // create grid of divs
-    for(let i = 0; i<16; ++i){
+    for(let i = 0; i<gridSize; ++i){
         let row = document.createElement('div');
         row.classList.add('row');
 
-        for(let j = 0; j<16; ++j){
+        for(let j = 0; j<gridSize; ++j){
             let box = document.createElement('div');
             box.classList.add('box');
             box.setAttribute('style', `width: ${boxSize}px; height: ${boxSize}px`)
@@ -22,6 +43,8 @@ function createGrid(){
 
         container.appendChild(row);
     }
+
+    document.body.appendChild(container);
 
     // add mouseover to all of the boxes
     let boxes = Array.from(document.getElementsByClassName('box'));
